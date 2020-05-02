5099400d0ea94832d87febfa30287193d56351da
Minh Huy <nnmhuy@apcs.vn>
2020-05-02 14:33:59 +0700

    Initial Commit

---
+++ Lab02_Object_Coloring/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab02_Object_Coloring
+//
+//  Created by Huy Nguyen Ngoc Minh on 5/2/20.
+//  Copyright © 2020 Huy Nguyen Ngoc Minh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
