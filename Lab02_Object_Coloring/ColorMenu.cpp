9ce76f730d7e0942965185f3e397e8f70f20f10c
Minh Huy <nnmhuy@apcs.vn>
2020-05-03 01:10:47 +0700

    draw and color shapes, bug cancel mouse after coloring

---
+++ Lab02_Object_Coloring/ColorMenu.cpp
@@ -0,0 +1,92 @@
+//
+//  ColorMenu.cpp
+//  Lab02_Object_Coloring
+//
+//  Created by Huy Nguyen Ngoc Minh on 5/2/20.
+//  Copyright © 2020 Huy Nguyen Ngoc Minh. All rights reserved.
+//
+
+#include "ColorMenu.hpp"
+
+const RGBColor colorsTable[3] = {
+    RGBColor(1, 0, 0), // red
+    RGBColor(0, 1, 0), // green
+    RGBColor(0, 0, 1)  // blue
+};
+
+const int dx[4] = {-1, 0, 1, 0};
+const int dy[4] = {0, -1, 0, 1};
+
+float * Ptr = new float [3 * (Config::MAX_COORDINATE + 1) *(Config::MAX_COORDINATE + 1)];
+
+void ColorMenu::getMouseClickPosition(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        glutMouseFunc(NULL);
+        cout << "Color from point: " << x << " " << y << endl;
+        ColorMenu::p = Point(x, y);
+        executeColor();
+    } else {
+        glutMouseFunc(NULL);
+        cout << "Cancelled color" << endl;
+    }
+}
+
+void ColorMenu::handleToggleMenu(int color){
+    cout << "COLOR MENU" << endl;
+    
+    // 0: red, 1: green, 2: blue
+    ColorMenu::currentColor = color - 1;
+    glutMouseFunc(getMouseClickPosition);
+
+}
+
+void ColorMenu::executeColor() {
+    glReadPixels(0, 0, Config::MAX_COORDINATE, Config::MAX_COORDINATE, GL_RGB, GL_FLOAT, Ptr);
+    RGBColor oldColor = getPixel(p);
+    RGBColor newColor = colorsTable[currentColor];
+
+    cout << "finished get all colors" << endl;
+    
+    queue <Point> que;
+    putPixel(p, newColor);
+    que.push(p);
+    while (!que.empty()) {
+        Point f = que.front();
+        que.pop();
+        for (int i = 0; i < 4; ++i) {
+            Point F;
+            F.x = f.x + dx[i];
+            F.y = f.y + dy[i];
+            if (F.x < 0 || F.x > Config::MAX_COORDINATE || F.y < 0 || F.y > Config::MAX_COORDINATE) continue;
+
+            if (getPixel(F) == oldColor) {
+                putPixel(F, newColor);
+                que.push(F);
+            }
+        }
+    }
+
+    glFlush();
+    cout << "done coloring" << endl;
+}
+
+RGBColor ColorMenu::getPixel(Point &p) {
+    int d = ((Config::MAX_COORDINATE - p.y) * Config::MAX_COORDINATE + p.x ) * 3;
+    return RGBColor(Ptr[d], Ptr[d + 1], Ptr[d + 2]);
+}
+
+void ColorMenu::putPixel(Point &p, RGBColor &color) {
+    float * ptr = new float [3];
+    ptr[0] = color.red;
+    ptr[1] = color.green;
+    ptr[2] = color.blue;
+    
+    int d = ((Config::MAX_COORDINATE - p.y) * Config::MAX_COORDINATE + p.x ) * 3;
+    Ptr[d] = color.red;
+    Ptr[d + 1] = color.green;
+    Ptr[d + 2] = color.blue;
+    
+    glRasterPos2i(p.x, p.y);
+    glDrawPixels(1, 1, GL_RGB, GL_FLOAT, ptr);
+    delete [] ptr;
+}
--- Lab02_Object_Coloring/main.cpp
+++ Lab02_Object_Coloring/main.cpp
@@ -6,10 +6,107 @@
 //  Copyright © 2020 Huy Nguyen Ngoc Minh. All rights reserved.
 //
 
+#define GL_SILENCE_DEPRECATION
 #include <iostream>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/GLUT.h>
+
+#include "Config.hpp"
+#include "Point.hpp"
+#include "ColorMenu.hpp"
+#include "DrawMenu.hpp"
+#include "Polygon.hpp"
+
+using namespace std;
+
+int window;
+int value;
+
+void RenderScreen(void) {
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Flush drawing commands
+    glFlush();
+}
+
+//  Set up the rendering state, set only once before rendering
+void SetupRC(void) {
+    // Set clear color to black
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glMatrixMode(GL_PROJECTION);
+}
+
+void mainMenu(int num){
+    cout << "MAIN MENU" << endl;
+    if(num == 0){
+        glutDestroyWindow(window);
+        exit(0);
+    } else {
+        value = num;
+    }
+    glutPostRedisplay();
+}
+
+void createMenu(void){
+    int draw_submenu_id = glutCreateMenu(DrawMenu::handleToggleMenu);
+    glutAddMenuEntry("Ellipse", 1);
+    glutAddMenuEntry("Rectangle", 2);
+    glutAddMenuEntry("Polygon", 3);
+    
+    int color_submenu_id = glutCreateMenu(ColorMenu::handleToggleMenu);
+    glutAddMenuEntry("Red", 1);
+    glutAddMenuEntry("Green", 2);
+    glutAddMenuEntry("Blue", 3);
+    
+    Config::main_menu_id = glutCreateMenu(mainMenu);
+    glutAddSubMenu("Draw", draw_submenu_id);
+    glutAddSubMenu("Color", color_submenu_id);
+    glutAddMenuEntry("Exit", 0);
+    
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+
+// Called by GLUT library when the window has changed size
+void ChangeSize(GLsizei w, GLsizei h)
+{
+    // Set Viewport to window dimensions
+    // glViewport(x, y, w, h) => (x,y): lower-left point of viewport; (w, h): width, height
+    glViewport(0, 0, w, h);
+    // Reset coordinate system
+    glMatrixMode(GL_PROJECTION);
+    
+    glLoadIdentity();
+
+    glOrtho(0, w, h, 0, 1, -1);
+    // all future transformation will affect model
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+Point ColorMenu::p;
+int ColorMenu::currentColor = 1;
+int DrawMenu::shape = 0;
+Point DrawMenu::start_point;
+Point DrawMenu::end_point;
+vector <Point> Polygon::currentPoints;
+int Config::main_menu_id;
+
+int main(int argc, char * argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowSize(Config::MAX_COORDINATE, Config::MAX_COORDINATE);
+    window = glutCreateWindow("Coloring Objects");
+    
+    SetupRC();
+    createMenu();
+    glutDisplayFunc(RenderScreen);
+    glutReshapeFunc(ChangeSize);
+
+    glutMainLoop();
     return 0;
 }
